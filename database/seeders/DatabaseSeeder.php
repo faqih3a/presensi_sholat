06322226422525555fdaa2a9b197565f9034aa93
faqih3a <ahmadfaqih3a@gmail.com>
2026-06-08 01:54:20 +0700

    fix: register SuperAdminSeeder in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            SuperAdminSeeder::class,
             AsatidzSeeder::class,
             SantriSeeder::class,
         ]);
